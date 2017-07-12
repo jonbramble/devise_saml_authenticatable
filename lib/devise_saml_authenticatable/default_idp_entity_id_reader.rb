7f711f16783f440a669f5409db9a38bf907d5db4
Jonathan Bramble <jonathan.bramble@ubc.ca>
2017-07-12 15:10:26 -0700

    updated settings for errors generating saml response with no idp entity

--- lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
@@ -1,15 +1,16 @@
 module DeviseSamlAuthenticatable
   class DefaultIdpEntityIdReader
+    include DeviseSamlAuthenticatable::SamlConfig
     def self.entity_id(params)
       if params[:SAMLRequest]
         OneLogin::RubySaml::SloLogoutrequest.new(
           params[:SAMLRequest],
+          settings: saml_config,
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuer
       elsif params[:SAMLResponse]
         OneLogin::RubySaml::Response.new(
           params[:SAMLResponse],
-	  settings: saml_config,
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuers.first
       end
