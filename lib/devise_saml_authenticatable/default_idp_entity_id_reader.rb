856717e646445052332ee25bf51c036448cbf1e9
Jonathan Bramble <jonathan.bramble@ubc.ca>
2017-07-12 12:31:07 -0700

    more issues with missing settings

--- lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
+++ lib/devise_saml_authenticatable/default_idp_entity_id_reader.rb
@@ -9,6 +9,7 @@ module DeviseSamlAuthenticatable
       elsif params[:SAMLResponse]
         OneLogin::RubySaml::Response.new(
           params[:SAMLResponse],
+	  settings: saml_config,
           allowed_clock_drift: Devise.allowed_clock_drift_in_seconds,
         ).issuers.first
       end
